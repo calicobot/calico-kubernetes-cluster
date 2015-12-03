11ef1ee91857cb6a2a6ae82d16474d51033d551d
metaswitchbot <daniel@projectcalico.org>
2015-12-03 15:52:43 -0800

    MINION->NODE, upstream binary host

--- test-cluster.sh
+++ test-cluster.sh
@@ -22,10 +22,7 @@ git fetch --all
 git checkout casey/calico-vagrant-integration
 cd ..
 
-cd ~/jobs/Calico-Kubernetes-PR/workspace/dist
-python3 -m http.server 8472 &
-cd ~/jobs/Kubernetes-Cluster/workspace
-
+# Binary should be built and hosted upstream.
 export ARTIFACT_URL=http://127.0.0.1:8472/calico_kubernetes
 wget $ARTIFACT_URL
 export ARTIFACT_SHA=$(sha512sum ./calico_kubernetes)
@@ -33,12 +30,12 @@ sudo rm calico_kubernetes
 sed -i "s/https:\/\/github.com\/projectcalico\/calico-kubernetes\/releases\/download\/v0.6.1\/calico_kubernetes/${ARTIFACT_URL}/" kubernetes/cluster/saltbase/salt/calico/node.sls
 sed -i "s/38d1ae62cf2a8848946e0d7442e91bcdefd3ac8c2167cdbc6925c25e5eb9c8b60d1f348eb861de34f4167ef6e19842c37b18c5fc3804cfdca788a65d625c5502/${ARTIFACT_SHA}/" kubernetes/cluster/saltbase/salt/calico/node.sls
 
-NUM_MINIONS=2 KUBE_VERSION=v1.1.1 KUBERNETES_PROVIDER=vagrant NETWORK_PROVIDER=calico kubernetes/cluster/kube-up.sh
+NUM_NODES=2 KUBE_VERSION=v1.1.1 KUBERNETES_PROVIDER=vagrant NETWORK_PROVIDER=calico kubernetes/cluster/kube-up.sh
 
-WORKERS=2; sed -i "s/NUM_MINIONS=[0-9]/NUM_MINIONS=${WORKERS}/" kubernetes/hack/conformance-test.sh
+WORKERS=2; sed -i "s/NUM_NODES=[0-9]/NUM_NODES=${WORKERS}/" kubernetes/hack/conformance-test.sh
 KUBECONFIG=/var/lib/jenkins/.kube/config kubernetes/hack/conformance-test.sh 2>&1 | tee conformance.$(date +%FT%T%z).log
 
-NUM_MINIONS=2 KUBE_VERSION=v1.1.1 KUBERNETES_PROVIDER=vagrant NETWORK_PROVIDER=calico kubernetes/cluster/kube-down.sh
+NUM_NODES=2 KUBE_VERSION=v1.1.1 KUBERNETES_PROVIDER=vagrant NETWORK_PROVIDER=calico kubernetes/cluster/kube-down.sh
 
 # Close calico_kubernetes http server
 ps axf | grep "python3 -m http.server 8472" | grep -v grep | awk '{print "kill -9 " $1}' | sh
